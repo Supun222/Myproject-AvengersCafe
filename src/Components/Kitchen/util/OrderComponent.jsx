92c89c6b463f624ed2196cecb4a01d8690214a9d
Dumindu Sachinthana <dumi.criss5952@gmail.com>
2021-11-14 17:47:59 +0530

    functions in kitchen

--- src/Components/Kitchen/util/OrderComponent.jsx
+++ src/Components/Kitchen/util/OrderComponent.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./OrderStyles.css";
 import Slider from "react-slick";
 import OrderCardComponent from "./OrderCardComponent";
+import { useState, useEffect } from "react/cjs/react.development";
+import axios from "../../../axios";
 
 function OrderComponent() {
   var settings = {
@@ -10,21 +12,61 @@ function OrderComponent() {
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
-    adaptiveHeight:true,
+    adaptiveHeight: true,
   };
 
+  const [selectedCustomerId, setSelectedCustomerId] = useState("");
+  const [ordersList, setOrdersList] = useState([]);
+  const getAllOrders = async () => {
+    await axios
+      .get("/api/order/cofirmorder")
+      .then((response) => {
+        response.data.map((data) => {
+          return setOrdersList((previousOrder) => [...previousOrder, data]);
+        });
+      })
+      .catch(function (error) {
+        if (error.response) {
+          console.error(error.response);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log(error);
+        }
+      });
+  };
+
+  useEffect(() => {
+    setOrdersList([]);
+    getAllOrders();
+  }, []);
   return (
     <>
-      <div style={{maxWidth: "90vw",}}>
+      <div style={{ maxWidth: "90vw" }}>
         <Slider {...settings}>
-          <OrderCardComponent key={1}/>
-          <OrderCardComponent key={2}/>
-          <OrderCardComponent key={3}/>
-          <OrderCardComponent key={4}/>
-          <OrderCardComponent key={5}/>
-          <OrderCardComponent key={6}/>
-          <OrderCardComponent key={7}/>
+          {ordersList.map((data, index) => {
+            return (
+              <button
+                onClick={() => {
+                  setSelectedCustomerId(data.cusID);
+                }}
+              >
+                <OrderCardComponent
+                  key={index}
+                  userData={data}
+                  selectedCustomerId={selectedCustomerId}
+                />
+              </button>
+            );
+          })}
         </Slider>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-3 done-btn-property">
+          <button type="button" className="btn btn-primary" disabled={(selectedCustomerId === "" || selectedCustomerId === undefined)}>
+            Done
+          </button>
+        </div>
       </div>
     </>
   );
