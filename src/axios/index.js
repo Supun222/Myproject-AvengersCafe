dc66900d7df81925ed9fd7aab8194cc5410741f0
Supun222 <74459467+Supun222@users.noreply.github.com>
2021-11-25 20:18:15 +0530

    Axios changed

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: "http://avengerscafebackend-env.eba-4k2yn9bw.us-east-2.elasticbeanstalk.com"
 })
 
 export default instance;
